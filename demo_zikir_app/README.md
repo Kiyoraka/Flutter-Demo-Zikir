67332404317d2d672fac5a52c7e358e8d7dcce2a
Afif Maahi <94098971+Kiyoraka@users.noreply.github.com>
2025-05-25 16:31:34 +0800

    initial commit

---
+++ demo_zikir_app/README.md
@@ -0,0 +1,3 @@
+# demo_zikir_app
+
+A new Flutter project.
---
+++ demo_zikir_app/android/app/src/main/kotlin/com/example/demo_zikir_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.demo_zikir_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
